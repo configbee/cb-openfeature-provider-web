3045a611f9d849d2ef4846fd34e3848d6e84ce1d
Sri Venkata Reddy <3039610+srivenkatareddy@users.noreply.github.com>
2026-01-25 19:25:30 +0530

    nx upgrade from 21 to 22

--- src/lib/configbee-web-provider.spec.ts
+++ src/lib/configbee-web-provider.spec.ts
@@ -1,7 +1,6 @@
 import { ConfigbeeWebProvider } from './configbee-web-provider';
-
 describe('ConfigbeeWebProvider', () => {
-  it('should be and instance of ConfigbeeWebProvider', () => {
-    expect(new ConfigbeeWebProvider({accountId: "myAccountId"})).toBeInstanceOf(ConfigbeeWebProvider);
-  });
+    it('should be and instance of ConfigbeeWebProvider', () => {
+        expect(new ConfigbeeWebProvider({ accountId: "myAccountId" })).toBeInstanceOf(ConfigbeeWebProvider);
+    });
 });
